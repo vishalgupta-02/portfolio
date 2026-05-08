219f40ad8ae049eed0f4dfbbe076c3e9c0ce09bd
CodeMonkey <abhimanyug987@gmail.com>
2026-05-08 21:49:31 +0530

    fix: og-image

--- src/app/blog/[slug]/layout.tsx
+++ src/app/blog/[slug]/layout.tsx
@@ -1,8 +1,58 @@
 import type { Metadata } from "next"
 import { ReactNode } from "react"
+import { allPosts } from "content-collections"
+import { DATA } from "@/data/resume"
 
-export const metadata: Metadata = {
-  title: "Blog Post | 2AM",
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{
+    slug: string
+  }>
+}): Promise<Metadata> {
+  const { slug } = await params
+  const post = allPosts.find((p) => p._meta.path.replace(/\.mdx$/, "") === slug)
+
+  if (!post) {
+    return {
+      title: "Blog Post | 2AM",
+    }
+  }
+
+  const {
+    title,
+    publishedAt: publishedTime,
+    summary: description,
+    image,
+  } = post
+
+  return {
+    title: `${title} | 2AM Blog`,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: "article",
+      publishedTime,
+      url: `${DATA.url}/blog/${slug}`,
+      authors: [DATA.name],
+      images: [
+        {
+          url: `${DATA.url}/blog/${slug}/opengraph-image`,
+          width: 1200,
+          height: 630,
+          alt: title,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      creator: "@vishalgupta_02",
+      images: [`${DATA.url}/blog/${slug}/opengraph-image`],
+    },
+  }
 }
 
 export default function BlogPostLayout({ children }: { children: ReactNode }) {
